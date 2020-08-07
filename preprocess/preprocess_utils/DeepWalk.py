c5e025a951a114496909d7b5c32026cfa4a43f09
panai <panagiotis.stasin@gmail.com>
2020-08-07 21:23:21 +0300

    7/8

--- preprocess/preprocess_utils/DeepWalk.py
+++ preprocess/preprocess_utils/DeepWalk.py
@@ -1,7 +1,14 @@
 import os
 from numpy.random import choice
 import pandas
+from numpy import array, argmax
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
 
+# for skip_gram
+from tensorflow.keras.models import Sequential
+from tensorflow.keras import layers
+from tensorflow.keras.optimizers import Adam
 
 # keeps only the name of each location visited
 def deep_walk(weighted_graph, distance_type, num_of_steps, num_of_walks, window_size):
@@ -30,13 +37,6 @@ def deep_walk(weighted_graph, distance_type, num_of_steps, num_of_walks, window_
             count = count + 1
 
     df = pandas.DataFrame.from_dict(vectors_set, orient='index')
-    # temp = []
-    # vectors_len = len(vectors_set[next(iter(vectors_set))])
-    # for j in range(1, vectors_len):
-    #     temp.append('feature' + str(j))
-    # temp.append('label')
-    # df.columns = temp
-    # df.rows = None
     df.to_csv(path + 'random_walks.csv', index=False)
     # df.to_json(path + 'random_walks.json', orient='index')
 
@@ -61,3 +61,48 @@ def find_next(neighbors):
         if draw.item(0) == na:
             break
     return draw.item(0), osID, center, os_type, area
+
+
+def skip_gram(weighted_graph, distance_type, num_of_steps, num_of_walks, window_size):
+    file = "../datasets/locations_csv/locations.csv"
+    df = pandas.read_csv(file)
+
+    temp = []
+    for index, row in df.iterrows():
+        temp.append(row['name'])
+
+    values = array(temp)
+    print(values.shape)
+    print(values)
+
+    # integer encode
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(values)
+    print(integer_encoded.shape)
+    print(integer_encoded)
+
+    # binary encode
+    onehot_encoder = OneHotEncoder(sparse=False)
+    integer_encoded = integer_encoded.reshape(len(integer_encoded), 1)
+    onehot_encoded = onehot_encoder.fit_transform(integer_encoded)
+    print(onehot_encoded.shape)
+    print(onehot_encoded)
+
+    # invert first example
+    inverted = label_encoder.inverse_transform([argmax(onehot_encoded[0, :])])
+    print(inverted.shape)
+    print(inverted)
+
+    model = Sequential([
+        layers.Dense(100, input_shape=(19648,)),
+        layers.Dense(4, activation="softmax")
+    ])
+    model.summary()
+    opt1 = Adam(learning_rate=.00335888)
+    model.compile(optimizer=opt1
+                  # , loss='sparse_categorical_crossentropy'
+                  , loss='categorical_crossentropy'
+                  # , metrics=['loss']
+    )
+
+    # model.fit(onehot_encoded, batch_size=78, epochs=20, shuffle=True)
--- preprocess/skip_gram.py
+++
@@ -1,7 +0,0 @@
-import time
-import utils
-
-if __name__ == "__main__":
-    start = time.time()
-
-    utils.show_exec_time(start)
